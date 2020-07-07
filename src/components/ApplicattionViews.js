debcf90c1d0e285bfc8c92914efbf6b4fe35079c
Trey Suiter <trey.suiter@gmail.com>
2020-07-07 14:54:11 -0500

    react router dom installed; hello printing to dom

---
+++ src/components/ApplicattionViews.js
@@ -0,0 +1,12 @@
+import { Route } from "react-router-dom";
+import React, { Component } from "react";
+
+const ApplicationViews = () => {
+    return (
+        <React.Fragment>
+            hello
+        </React.Fragment>
+    )
+}
+
+export default ApplicationViews
---
+++ src/components/NSSSE.js
@@ -0,0 +1,14 @@
+import React from "react";
+// import NavBar from "./nav/NavBar";
+import ApplicationViews from "./ApplicattionViews";
+
+const NSSSE = () => {
+  return (
+    <>
+      {/* <NavBar /> */}
+      <ApplicationViews />
+    </>
+  );
+};
+
+export default NSSSE;
